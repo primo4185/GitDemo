3e508365586de81688be8470c8fe96704d99f8ed
primo4185 <41276559+primo4185@users.noreply.github.com>
2018-07-17 15:51:20 +0900

    파이버벳

--- Starcraft/StarCraft/StarCraft/Program.cs
+++ Starcraft/StarCraft/StarCraft/Program.cs
@@ -11,17 +11,25 @@ namespace StarCraft
         static void Main(string[] args)
         {
             Marine m1 = new Marine();
-            int hp1 = UseSteamPack(m1, 2);
+            int hp1 = Marine_UseSteamPack(m1, 2);
 
             Marine m2 = new Marine();
-            int hp2 = UseSteamPack(m2, 1);
+            int hp2 = Marine_UseSteamPack(m2, 1);
 
             Heal(m1, 30);
 
-            GetDamagd(m2, 3);
+            Marine_GetDamagd(m2, 3);
+            Firebat f1 = new Firebat();
+            Firebat_UseSteamPack(f1, 1);
+            Firebat_GetDamagd(f1, 3);
         }
 
-        private static void GetDamagd(Marine m, int enemyAttack)
+        private static void Firebat_GetDamagd(Firebat f1, int enemyAttack)
+        {
+            f1.Hp -= (enemyAttack * 3);
+        }
+
+        private static void Marine_GetDamagd(Marine m, int enemyAttack)
         {
             m.Hp -= (enemyAttack * 2);
         }
@@ -36,21 +44,37 @@ namespace StarCraft
     }
 
     return m.Hp; */
-        static int UseSteamPack(Marine m, int count)
+        static int Marine_UseSteamPack(Marine m, int count)
         {
             for (int i = 0; i < count; i++)
             {
-                UseSteamPackCore(m);
+                Marine_UseSteamPackCore(m);
 
             }
 
             return m.Hp;
         }
-        static void UseSteamPackCore(Marine m)
+        static int Firebat_UseSteamPack(Firebat f, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Firebat_UseSteamPackCore(f);
+
+            }
+
+            return f.Hp;
+        }
+        static void Marine_UseSteamPackCore(Marine m)
         {
             m.Hp = m.Hp - 5;
             m.Speed = m.Speed + 1;
         }
+        static void Firebat_UseSteamPackCore(Firebat f)
+        {
+            f.Hp = f.Hp - 5;
+            f.Speed = f.Speed + 1;
+        }
+
         static void Heal(Marine m, int hpToAdd)
         {
             //m.Hp = m.Hp + hpToAdd;
